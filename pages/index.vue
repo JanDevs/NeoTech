ea9120cd5018d929541fde7d280e64db63506882
JanDevs <janlolsas272272@gmail.com>
2023-05-29 09:00:59 -0600

    Landing page

---
+++ pages/index.vue
@@ -0,0 +1,468 @@
+<template>
+  <div class="w-full">
+    <!-- Hero section -->
+    <section id="hero" class="w-full pb-24">
+      <BaseSection>
+        <div class="col-span-12 lg:col-span-6 mt-12 xl:mt-10 space-y-4 sm:space-y-6 px-6 text-center sm:text-left">
+          <span data-aos="fade-right" data-aos-once="true" class="text-base text-gradient font-semibold uppercase"
+            >Regístrate ahora</span
+          >
+          <h1
+            data-aos="fade-right"
+            data-aos-once="true"
+            class="text-[2.5rem] sm:text-5xl xl:text-6xl font-bold leading-tight capitalize sm:pr-8 xl:pr-10"
+          >
+            La mejor aplicación de <span class="text-header-gradient">Análisis musical </span> Del mundo
+          </h1>
+          <p data-aos="fade-down" data-aos-once="true" data-aos-delay="300" class="paragraph hidden sm:block">
+            Analiza cualquier canción en diversos formatos, obtén su ritmo, su tonalidad y mucho más. Todo desde una misma aplicación.
+          </p>
+          <div
+            data-aos="fade-up"
+            data-aos-once="true"
+            data-aos-delay="700"
+            class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 mt-2"
+          >
+            <BaseButton
+              class="max-w-full px-8 py-4 bg-gradient-to-r from-[#468ef9] to-[#0c66ee] border border-[#0c66ee] text-white"
+            >
+              Acceder 
+            </BaseButton>
+            
+          </div>
+        </div>
+        <div class="hidden sm:block col-span-12 lg:col-span-6">
+          <div class="w-full">
+            <img
+              data-aos="fade-up"
+              data-aos-once="true"
+              :src="require('~/assets/img/hero-image.webp')"
+              class="-mt-4"
+              alt=""
+              style="border-radius: 1000px;"
+            />
+          </div>
+        </div>
+        <img
+          data-aos="fade-up"
+          data-aos-delay="300"
+          :src="require('~/assets/img/pattern/ellipse-1.png')"
+          class="hidden sm:block absolute bottom-12 xl:bottom-16 left-4 xl:left-0 w-6"
+        />
+        <img
+          data-aos="fade-up"
+          data-aos-delay="300"
+          :src="require('~/assets/img/pattern/ellipse-2.png')"
+          class="hidden sm:block absolute top-4 sm:top-10 right-64 sm:right-96 xl:right-[32rem] w-6"
+        />
+        <img
+          data-aos="fade-up"
+          data-aos-delay="300"
+          :src="require('~/assets/img/pattern/ellipse-3.png')"
+          class="hidden sm:block absolute bottom-56 right-24 w-6"
+        />
+        <img
+          data-aos="fade-up"
+          data-aos-delay="300"
+          :src="require('~/assets/img/pattern/star.png')"
+          class="hidden sm:block absolute top-20 sm:top-28 right-16 lg:right-0 lg:left-[30rem] w-8"
+        />
+      </BaseSection>
+    </section>
+
+    <!-- Buy and trade section -->
+    <section class="w-full my-24">
+      <BaseSection>
+        <LandingBuyTradeImage class="sm:hidden mb-8" />
+        <div data-aos="fade-right" class="col-span-12 lg:col-span-6 mt-4 xl:mt-20 space-y-6 px-4">
+          <h2 class="text-4xl font-semibold sm:pr-8 xl:pr-12">
+            ¡Suscríbete ya <br class="hidden sm:block" />
+            a la mejor aplicación de análisis!
+          </h2>
+          <p class="paragraph">
+            Toma la suscripción de 12 meses y ahorra dinero
+          </p>
+          <div class="space-y-6 lg:pr-12">
+            <LandingExchange
+              title="Suscripción de 12 meses"
+              name="12meses"
+              type="number"
+              default-value="999"
+              :exchange-selected="currencySelected"
+              :exchanges="currencies"
+            />
+            <LandingExchange
+              title="Suscripción de 1 mes"
+              name="1mes"
+              type="number"
+              default-value="999"
+              :exchange-selected="cryptoSelected"
+              :exchanges="cryptocurrencies"
+            />
+            <BaseButton class="w-full px-5 py-4 bg-blue-gradient text-white text-base font-medium">Suscribirse ahora</BaseButton>
+          </div>
+        </div>
+        <LandingBuyTradeImage data-aos="fade-left" class="hidden sm:block" />
+      </BaseSection>
+    </section>
+
+    <!-- Partners section -->
+    <section class="bg-partner relative max-w-full sm:mx-6 my-24 shadow sm:rounded-2xl overflow-hidden">
+      <div class="w-full px-6 sm:px-0 py-16 flex flex-col items-center justify-center space-y-4 text-center">
+        <h3 data-aos="flip-down" class="text-2xl text-neutral-800 font-semibold">Estamos asociados con grandes empresas</h3>
+        <p data-aos="flip-down" class="paragraph">Somos reconocidos por la industria musical</p>
+        <div data-aos="fade-up" class="flex flex-wrap items-center justify-center">
+          <LandingPartnerImage
+            v-for="img in ['spotify.png', 'sony.png', 'warner.png']"
+            :key="img"
+            :img="img"
+          />
+        </div>
+      </div>
+    </section>
+
+    <!-- Credit card section -->
+    <section class="w-full my-36">
+      <BaseSection data-aos="fade-down">
+        <div class="col-span-12 lg:col-span-7">
+          <div class="w-full">
+            <img :src="require('~/assets/img/nefa-cc.webp')" class="w-[95%]" alt="" />
+          </div>
+        </div>
+        <div class="col-span-12 lg:col-span-5 space-y-6 px-4 sm:px-6 mt-20">
+          <h2 class="text-4xl font-semibold">
+            Una herramienta<span class="text-header-gradient">confiable</span>
+          </h2>
+          <p class="paragraph">Nuestra aplicación utiliza la inteligencia artificial para dar los mejores resultados</p>
+          <ul class="space-y-4 sm:space-y-2">
+            <LandingListItem title="Resultados en menos de 1 minuto" />
+            <LandingListItem title="Gran precisión" />
+            <LandingListItem title="Siempre disponible" />
+          </ul>
+          <BaseButton
+            class="w-full sm:max-w-[240px] px-10 py-4 bg-inherit text-gradient border border-[#0c66ee] text-base"
+            >Únete a los más de 10000 usuarios</BaseButton
+          >
+        </div>
+      </BaseSection>
+    </section>
+
+    <!-- Advanced trading tools section -->
+    <section class="bg-trading-tools relative max-w-full sm:mx-4 my-20 py-16 shadow rounded-2xl overflow-hidden">
+      <div class="relative max-w-screen-xl px-4 sm:px-2 mx-auto grid grid-cols-12 gap-x-6">
+        <LandingTradingToolImage class="sm:hidden" />
+        <div data-aos="fade-right" class="col-span-12 lg:col-span-6 space-y-8 sm:space-y-6 px-4 sm:px-6 mt-8">
+          <h2 class="text-4xl font-semibold">Siente el<span class="text-header-gradient"> poder</span></h2>
+          <div class="space-y-2">
+            <h4 class="text-lg font-medium">Disponibilidad ininterrumpida</h4>
+            <p class="paragraph text-sm xl:text-base">
+              Ofrecemos el mejor servicio, asegurando que siempre se encontrará disponible las 24 horas del día.
+            </p>
+          </div>
+          <div class="space-y-2">
+            <h4 class="text-lg font-medium">Una gran variedad de fuentes musicales</h4>
+            <p class="paragraph text-sm xl:text-base">
+              Tenemos muchas empresas que nos proveen de un enorme catálogo musical, al que tendrás acceso con un clic.
+            </p>
+          </div>
+          <div class="space-y-2">
+            <h4 class="text-lg font-medium">Soporte al cliente</h4>
+            <p class="paragraph text-sm xl:text-base">
+              Soporte técnico disponible las 24 horas, los 7 días de la semana para todos nuestros clientes. Pueden contactarnos mediante correo electrónico o celular.
+            </p>
+          </div>
+          <div class="flex flex-col sm:flex-row">
+            <BaseButton class="px-10 py-4 bg-inherit text-gradient border border-[#4A8FF6] text-base">
+              Quiero registrarme
+            </BaseButton>
+            
+          </div>
+        </div>
+        <LandingTradingToolImage data-aos="fade-left" class="hidden sm:block" />
+      </div>
+    </section>
+
+    <!-- Industry-leading security section -->
+    <section class="w-full my-24">
+      <div class="relative max-w-screen-xl px-8 mx-auto grid grid-cols-12 gap-x-6">
+        <div data-aos="fade-right" class="col-span-12 lg:col-span-6">
+          <div class="w-full">
+            <img :src="require('~/assets/img/caja-fuerte.webp')" class="w-full" alt="" />
+          </div>
+        </div>
+        <div data-aos="fade-left" class="col-span-12 lg:col-span-5 space-y-8 sm:space-y-6 mt-8 xl:px-8">
+          <h2 class="text-4xl font-semibold">Tus datos protegidos desde el primer día</h2>
+          <ul class="space-y-8 sm:space-y-4">
+            <LandingListItem title="Seguridad y cumplimiento">
+              <p class="text-sm text-gray-700 leading-relaxed">
+                Usamos los métodos más seguros para proteger los datos de todos nuestros usuarios
+              </p>
+            </LandingListItem>
+            <LandingListItem title="Uso de azure">
+              <p class="text-sm text-gray-700 leading-relaxed">
+                Con el uso de Azure nos aseguramos de que los datos solo estén siendo utilizados por el sistema dentro de un espacio aislado.
+              </p>
+            </LandingListItem>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    <!-- Getting started section -->
+    <section class="bg-trading-tools relative max-w-full sm:mx-4 xl:mx-10 my-24 shadow sm:rounded-2xl overflow-hidden">
+      <div class="w-full py-16 flex flex-col items-center">
+        <h2 data-aos="flip-down" class="text-3xl sm:text-4xl font-semibold text-center">
+          Inicia en pocos minutos
+        </h2>
+        <div
+          data-aos="fade-up"
+          class="relative w-full flex flex-col lg:flex-row items-center justify-between space-y-12 lg:space-y-0 px-4 xl:px-10 mt-16 sm:mt-8"
+        >
+          <LandingStep v-for="step in steps" :key="step.title" :step="step" />
+          <img
+            :src="require('~/assets/img/getting-started/arrow.png')"
+            class="hidden lg:inline-block absolute top-32 left-64 xl:left-[22rem] w-24 xl:w-[9.5rem]"
+            alt=""
+          />
+          <img
+            :src="require('~/assets/img/getting-started/arrow.png')"
+            class="hidden lg:inline-block absolute top-32 right-64 xl:right-[22rem] w-24 xl:w-[9.5rem]"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+
+    <!-- FAQ section -->
+    <section class="w-full my-24">
+      <BaseSection>
+        <div data-aos="fade-right" data-aos-delay="150" class="col-span-12 lg:col-span-6">
+          <div class="w-full">
+            <img :src="require('~/assets/img/miku.webp')" class="w-full" alt="" />
+          </div>
+        </div>
+        <div data-aos="fade-left" data-aos-delay="150" class="col-span-12 lg:col-span-6 px-4 sm:px-6 mt-8">
+          <span class="text-base text-gradient font-semibold uppercase mb-4 sm:mb-2">Soporte</span>
+          <h2 class="text-3xl sm:text-4xl font-semibold mb-10 sm:mb-6">Preguntas frecuentes</h2>
+
+          <ul class="shadow-box">
+            <BaseAccordion v-for="(accordion, index) in accordions" :key="index" :accordion="accordion" />
+          </ul>
+        </div>
+      </BaseSection>
+    </section>
+
+    <div class="w-full my-10 flex justify-center">
+      <a
+        v-smooth-scroll
+        data-aos="flip-down"
+        data-aos-delay="150"
+        href="#navbar"
+        class="px-6 py-3 flex items-center space-x-2 bg-[#FAFAFA] hover:bg-gray-100 hover:shadow-md border border-[#DDDDDD] rounded-md text-gray-700"
+      >
+        <span>Volver arriba</span>
+        <ArrowUpIcon :size="20" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import aosMixin from '@/mixins/aos'
+export default {
+  name: 'IndexPage',
+  mixins: [aosMixin],
+  data() {
+    return {
+      selected: 0,
+      dropdownConcurency: false,
+      dropdownCrypto: false,
+      currencySelected: {
+        img: 'country-icon/eng.png',
+        name: 'USD',
+      },
+      currencies: [
+        {
+          img: 'country-icon/eng.png',
+          name: 'USD',
+        },
+      ],
+      cryptoSelected: {
+        img: 'crypto-icon/bitcoin.png',
+        name: 'BTC',
+      },
+      cryptocurrencies: [
+        {
+          img: 'crypto-icon/bitcoin.png',
+          name: 'BTC',
+        },
+      ],
+      trendings: [
+        {
+          id: 1,
+          name: 'Bitcoin',
+          price: 43180.13,
+          logo: 'bitcoin.png',
+          increase: true,
+          data: [40, 35, 60, 75, 60, 75, 50],
+        },
+        {
+          id: 2,
+          name: 'Ethereum',
+          price: 3480.65,
+          logo: 'ethereum.png',
+          increase: false,
+          data: [25, 30, 60, 50, 80, 55, 80],
+        },
+        {
+          id: 3,
+          name: 'Solana',
+          price: 150.2,
+          logo: 'solana.png',
+          increase: true,
+          data: [40, 45, 40, 80, 50, 60, 35],
+        },
+        {
+          id: 4,
+          name: 'Dogecoin',
+          price: 0.1572,
+          logo: 'dogecoin.png',
+          increase: true,
+          data: [35, 70, 60, 80, 50, 60, 40],
+        },
+      ],
+      topGainers: [
+        {
+          id: 1,
+          name: 'PAPPAY',
+          price: 0.00374,
+          logo: 'pappay.png',
+          increase: true,
+          data: [30, 50, 45, 60, 70, 40, 45],
+        },
+        {
+          id: 2,
+          name: 'Bitcoin Asia',
+          price: 0.02096,
+          logo: 'bitcoin-asia.png',
+          increase: true,
+          data: [25, 60, 50, 60, 35, 50, 70],
+        },
+        {
+          id: 3,
+          name: 'MoonRock',
+          price: 0.004907,
+          logo: 'moonrock.png',
+          increase: true,
+          data: [40, 35, 40, 25, 50, 70, 45],
+        },
+        {
+          id: 4,
+          name: 'NinjaFloki',
+          price: 0.000123,
+          logo: 'ninjafloki.png',
+          increase: true,
+          data: [45, 35, 40, 30, 25, 45, 35],
+        },
+      ],
+      recents: [
+        {
+          id: 1,
+          name: 'MetaCraft',
+          price: 0.0608,
+          logo: 'metacraft.png',
+          increase: false,
+          data: [40, 50, 45, 60, 35, 40, 45],
+        },
+        {
+          id: 2,
+          name: 'Frog',
+          price: 0.5875,
+          logo: 'frog.png',
+          increase: false,
+          data: [25, 50, 45, 48, 40, 60, 45],
+        },
+        {
+          id: 3,
+          name: 'Musk Doge',
+          price: 0.04041,
+          logo: 'musk-doge.png',
+          increase: true,
+          data: [25, 35, 60, 45, 50, 45, 45],
+        },
+        {
+          id: 4,
+          name: '2SHARE',
+          price: 1366.24,
+          logo: '2share.png',
+          increase: true,
+          data: [35, 30, 60, 50, 35, 45, 40],
+        },
+      ],
+      accordions: [
+        {
+          title: '¿Por qué debería usar su aplicación?',
+          description:
+            "Somos una industria con experiencia manejando la inteligencia artificial, por lo que te aseguramos una gran presición en los resultados.",
+        },
+        {
+          title: '¿Habrá cobros adicionales?',
+          description:
+            "No, una vez hayas pagado tu suscripción, podrás acceder a todas las funciones sin costo adicional.",
+        },
+        {
+          title: '¿Solo hay suscripción anual?',
+          description:
+            "Contamos con una suscripción anual y una mensual. Con la suscripción mensual puedes cancelar en cualquier momento y se detendrá el cobro periódico.",
+        },
+        {
+          title: '¿Su aplicación es difícil de usar?',
+          description:
+            "Contamos con una interfaz amigable para los usuarios e igualmente contamos con videos explicando su funcionamiento. En caso de tener más dudas, puedes contactarnos en cualquiera de nuestros medios de contacto.",
+        },
+      ],
+      steps: [
+        {
+          img: 'sign-up.png',
+          title: 'Regístrate',
+          description:
+            'Regístrate en nuestro sitio web y sigue el sencillo proceso para hacer tu cuenta',
+        },
+        {
+          img: 'fund.png',
+          title: 'Elige una suscripción',
+          description:
+            'Elige uno de nuestros planes y realiza el pago correspondiente, aceptamos todas las tarjetas',
+        },
+        {
+          img: 'analisis.png',
+          title: 'Empieza con el análisis',
+          description:
+            'Ingresa una canción y elige los criterios a analizar con nuestra interfaz amigable',
+        },
+      ],
+    }
+  },
+}
+</script>
+<style scoped>
+.text-header-gradient {
+  background: rgb(57, 132, 244);
+  background: linear-gradient(169.4deg, #3984f4 -6.01%, #0cd3ff 36.87%, #2f7cf0 78.04%, #0e65e8 103.77%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.bg-partner {
+  background: url('../assets/img/partner/background.png');
+  background-size: cover;
+  background-position: center;
+}
+.bg-trading-tools {
+  background: url('../assets/img/bg-trading-tools.webp');
+  background-size: cover;
+  background-position: center;
+}
+.max-h-0 {
+  max-height: 0;
+}
+</style>
